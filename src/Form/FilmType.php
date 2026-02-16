a6d5f57bf74ac3726c428de02713b975a1b6b87c
Yannick-lep <lepinayyannick7@gmail.com>
2026-02-16 09:31:53 +0100

    re*re

--- src/Form/FilmType.php
+++ src/Form/FilmType.php
@@ -6,6 +6,9 @@ use App\Entity\Film;
 use App\Entity\Platforme;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +17,25 @@ class FilmType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('synopsis')
-            ->add('releaseYear')
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['placeholder' => 'Ex: Inception']
+            ])
+            ->add('synopsis', TextareaType::class, [
+                'label' => 'Synopsis',
+                'required' => false,
+                'attr' => ['rows' => 5, 'placeholder' => 'Résumé du film...']
+            ])
+            ->add('releaseYear', IntegerType::class, [
+                'label' => 'Année de sortie',
+                'attr' => ['placeholder' => 'Ex: 2010']
+            ])
             ->add('platformes', EntityType::class, [
+                'label' => 'Plateformes disponibles',
                 'class' => Platforme::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
                 'multiple' => true,
+                'expanded' => true,
             ])
         ;
     }
